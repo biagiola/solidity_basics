033efd267850e40a2ae75db1f12ee446c80665ed
David <marcelobiagiola01@gmail.com>
2023-01-22 20:08:28 -0300

    added: mapping with casting value

---
+++ mappings_03.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <=0.8.17;
+
+contract Inventory {
+    mapping(uint => int) quantities;
+    
+    function addItem(uint256 itemId, uint256 quantity) public {
+        quantities[itemId] += int256(quantity);
+    }
+
+    function getQuantity(uint256 itemId) public view returns (int256) {
+        return quantities[itemId] != 0 ? int256(quantities[itemId]) : -1;
+    }
+}
